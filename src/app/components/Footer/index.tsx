8f4a220d6e79137bd172730849565720c1fe8b6c
hsk-kr <hsk.coder@gmail.com>
2023-11-12 00:01:23 +0100

    feat: add github icon in the footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const navItems = [
@@ -13,23 +14,36 @@ const navItems = [
 
 export default function Footer() {
   return (
-    <footer className="text-sm flex flex-col justify-center items-center bg-gray-100 text-gray-600 p-4 border-t-[1px] border-gray-300">
-      <h3 className="mb-2 text-center">
+    <footer className="text-sm flex flex-col gap-y-2 justify-center items-center bg-gray-100 text-gray-600 p-4 border-t-[1px] border-gray-300">
+      <h3 className="text-center">
         Languges are the key to communicating with the world and understanding
         yourself better.
       </h3>
-      <ul className="flex list-disc marker:text-blue-600 gap-x-6">
+      <ul className="flex list-disc marker:text-blue-600 gap-x-6 ">
         {navItems.map((navItem) => (
           <li key={navItem.label}>
             <Link
               href={navItem.href}
-              className="text-blue-600 text-sm hover:text-blue-400"
+              className="text-blue-600 text-sm hover:text-blue-400 cursor-pointer"
             >
               {navItem.label}
             </Link>
           </li>
         ))}
       </ul>
+      <Link
+        href="https://github.com/hsk-kr/nextjs-isr-example"
+        target="_blank"
+        className="flex gap-x-1 text-black cursor-pointer hover:text-gray-600"
+      >
+        <Image
+          width={20}
+          height={16}
+          src="/github-mark.svg"
+          alt="github repository"
+        />
+        <span>nextjs-isr-example</span>
+      </Link>
     </footer>
   );
 }
